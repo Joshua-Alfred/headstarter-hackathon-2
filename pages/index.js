d21192ebf1d5d27ad591cd2da358cdd14b176c98
Ken Lu <kenlu519@gmail.com>
2024-08-17 15:51:35 -0400

    Update front end colors

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,9 @@ import {
   TextField,
   CircularProgress,
   Card,
+  Paper,
   Stack,
+  Box,
   CardContent,
 } from "@mui/material";
 import DOMPurify from "dompurify";
@@ -50,32 +52,58 @@ export default function ScraperApp() {
 
   return (
     <Container maxWidth="md" style={{ marginTop: "50px" }}>
-      <Typography variant="h4" gutterBottom>
-        Web Scraper Tool
-      </Typography>
-      <TextField
-        label="Enter URL"
-        variant="outlined"
-        fullWidth
-        value={userUrl}
-        onChange={(e) => setUserUrl(e.target.value)}
-        style={{ marginBottom: "20px" }}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleClick}
-        disabled={loading}
-      >
-        {loading ? <CircularProgress size={24} /> : "Scrape"}
-      </Button>
+      <Stack spacing={2} sx={{ width: "80%", mx: "auto" }}>
+        <Typography variant="h4" gutterBottom sx={{ textAlign: "center" }}>
+          The Best{" "}
+          <Box sx={{ color: "#9563FF" }} component="span">
+            Web Scraping
+          </Box>{" "}
+          Tool
+        </Typography>
+        <Typography gutterBottom sx={{ textAlign: "center" }}>
+          Scrape a whole website of your choice
+        </Typography>
+        <Paper
+          elevation={2}
+          sx={{
+            p: 5,
+            border: "2px solid",
+            borderImage:
+              "linear-gradient(135deg, orange 0%, red 25%, purple 50%, blue 75%) 1",
+          }}
+        >
+          <Stack>
+            <TextField
+              label="Enter URL"
+              variant="outlined"
+              fullWidth
+              value={userUrl}
+              onChange={(e) => setUserUrl(e.target.value)}
+              style={{ marginBottom: "20px" }}
+            />
+            <Button
+              variant="contained"
+              onClick={handleClick}
+              disabled={loading}
+              sx={{
+                backgroundColor: "black",
+                "&:hover": {
+                  backgroundColor: "#8962dc",
+                },
+              }}
+            >
+              {loading ? <CircularProgress size={24} /> : "Scrape"}
+            </Button>
+          </Stack>
+        </Paper>
+      </Stack>
       {error && (
         <Typography color="error" style={{ marginTop: "20px" }}>
           {error}
         </Typography>
       )}
-      {htmlData && showRawData && <Typography>{htmlData}</Typography>}
-      {htmlData && !showRawData && (
+
+      {htmlData && (
         <>
           <Stack
             direction="row"
@@ -83,17 +111,24 @@ export default function ScraperApp() {
             justifyContent={"space-between"}
           >
             <Typography variant="h6">Scraped Data</Typography>
-            <Button onClick={() => setShowRawData(true)} variant="outlined">
+            <Button
+              onClick={() => setShowRawData(!showRawData)}
+              variant="outlined"
+            >
               View raw html
             </Button>
           </Stack>
           <Card style={{ marginTop: "20px" }}>
             <CardContent>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(htmlData),
-                }}
-              />
+              {showRawData ? (
+                <Typography>{htmlData}</Typography>
+              ) : (
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(htmlData),
+                  }}
+                />
+              )}
             </CardContent>
           </Card>
         </>
