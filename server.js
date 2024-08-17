642b5fd00953bec1a7ffe6ce68918b423eddeb8e
bloopgoop <zhu.kevin12@gmail.com>
2024-08-17 13:59:39 -0400

    connect to mongo atlas

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 const express = require('express');
-const next = require('next');
+const next = require('next')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+;
+require('dotenv').config({ path: '.env.local' });
+
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@headstarter-hackathon-2.3gjkh.mongodb.net/?retryWrites=true&w=majority&appName=headstarter-hackathon-2`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
