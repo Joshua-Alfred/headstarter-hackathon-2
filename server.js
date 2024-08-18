5649cd4ed66f080f6af0107326e0947b6520e2d9
Joshua-Alfred <joshua.jayapal@gmail.com>
2024-08-18 16:33:14 -0400

    removed executablepath

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.prepare().then(() => {
       const browser = await puppeteer.launch({
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        executablePath: executablePath(), 
+        
       });
       
       const page = await browser.newPage();
